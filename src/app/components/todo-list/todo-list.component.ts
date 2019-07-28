2d3e9f6ab7044f3002a9196941fccdafbb1a0e92
Sean Tayler <seantayler@gAM7acd39.local>
2019-07-27 22:11:03 -0700

    add todo

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
 
+  todos:object[];
+  todoTitle:string;
+  todoId:number = 2;
+
   constructor() { }
 
   ngOnInit() {
+    this.todoTitle = '';
+    this.todos = [{
+      'id': 1,
+      'title': 'finish todo app',
+      'completed': false,
+      'editing': false
+    }]
+  }
+
+  addTodo():void {
+    if(!this.todoTitle.trim().length){
+      return; 
+    }
+    this.todos.push({
+      id: this.todoId,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+    this.todoTitle = ''
+    this.todoId++; 
   }
 
 }
